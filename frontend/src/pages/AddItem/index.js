113332d9f934f7c7542098eb5c2d28b21fd55124
brenogsouza <breno.souza.developer@gmail.com>
2020-04-19 13:27:53 -0300

    ajustado bug nas rotas

--- frontend/src/pages/AddItem/index.js
+++ frontend/src/pages/AddItem/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-const AddItem = () => <div />;
+const AddItem = () => {
+  return (
+    <form className="container mt-5">
+			<div className="form-group">
+				<label>Nome</label>
+				<input type="text" name="name" className="form-control" placeholder="Nome..." />
+			</div>
+			<div className="form-group">
+				<label>URL:</label>
+				<input type="text" name="url" className="form-control" placeholder="URL:https://cars" />
+			</div>
+			<button type="submit" className="btn btn-primary">
+				Adicionar
+			</button>
+		</form>
+  )
+};
 
 export default AddItem;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import { Redirect, Route } from 'react-router-dom'
-import { AddItem, Cart, ListItem } from './pages'
-
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { AddItem, Cart, ListItem } from "./pages";
 
 const Routes = () => {
   return (
-<switch>
-  <Route path='/lista-item' component={ListItem} />
-  <Route path="adicionar-item" component={AddItem} />
-  <Route path="carrinho" component={Cart} />
-  <Redirect path="/" to="/lista-item" />
-</switch>
-  )
-}
+    <Switch>
+      <Route path="/lista-item" component={ListItem} />
+      <Route path="/adicionar-item" component={AddItem} />
+      <Route path="/carrinho" component={Cart} />
+      <Redirect path="/" to="/lista-item" />
+    </Switch>
+  );
+};
 
-export default Routes
+export default Routes;
